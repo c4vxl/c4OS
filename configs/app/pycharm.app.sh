3ef15aa166f9b0a61ae90904bfb79a2b823de8f1
c4vxl <manage@c4vxl.de>
2025-03-29 23:17:49 +0100

    added installer for intellij and pycharm

---
+++ configs/app/pycharm.app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NAME="pycharm-community-edition"
+SHORT="pycharm"
+yay -S $NAME --noconfirm
+
+# install to jetbrains toolbox (if toolbox is installed)
+TOOLBOX_PATH="/home/$(whoami)/.local/share/JetBrains/Toolbox"
+if [[ -d "$TOOLBOX_PATH" ]]; then
+    sudo mv /usr/share/$SHORT $TOOLBOX_PATH/apps/$NAME
+    sudo chmod 777 -R $TOOLBOX_PATH/apps/$NAME
+    ln -s $TOOLBOX_PATH/apps/$NAME/bin/$SHORT $TOOLBOX_PATH/scripts/$SHORT
+fi
