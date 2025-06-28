0cfd2f19e176acad92356d6375dcc23de7220030
c4vxl <manage@c4vxl.de>
2025-06-28 20:55:25 +0200

    added etcher and eyedropper to applications

---
+++ configs/app/eyedropper.app.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo pacman -S eyedropper --noconfirm
--- configs/app/steam.app.sh
+++ configs/app/steam.app.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
-sudo pacman -S steam --noconfirm
+sudo pacman -R amdvlk lib32-amdvlk --noconfirm
+sudo pacman -S vulkan-radeon lib32-vulkan-radeon steam --noconfirm
 
 cp /usr/share/applications/steam.desktop ~/Desktop/steam.desktop/steam.desktop
 sudo chmod +x ~/Desktop/steam.desktop/steam.desktop
