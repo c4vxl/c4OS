ef1edb61408bb0db99a8facb685356c2021ec768
c4vxl <manage@c4vxl.de>
2025-06-29 11:39:54 +0200

    added chromium to applications

---
+++ configs/app/chromium.app.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+yay -S google-chrome --noconfirm
+
+cp /usr/share/applications/chromium.desktop ~/Desktop/chromium.desktop
+sudo chmod +x ~/Desktop/chromium.desktop
--- configs/app/google_chrome.app.sh
+++ configs/app/google_chrome.app.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 sudo pacman -S chromium --noconfirm
 
-cp /usr/share/applications/chromium.desktop ~/Desktop/chromium.desktop
-sudo chmod +x ~/Desktop/chromium.desktop
+cp /usr/share/applications/google-chrome.desktop ~/Desktop/google-chrome.desktop
+sudo chmod +x ~/Desktop/google-chrome.desktop
